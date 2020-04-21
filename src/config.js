d093e8c473d8f32ff86e001505fe2ccd8f7d91fc
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2020-04-21 13:57:37 -0500

    ready for deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: 'postgresql://dunder_mifflin@localhost/reflect',
-    TEST_DB_URL: 'postgresql://dunder_mifflin@localhost/reflect_test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/reflect',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/reflect_test',
     CLIENT_ORIGIN: '*'
 }
